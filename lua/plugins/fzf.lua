6534811a68c9d99a4910d3d90f9c0855c16868e0
Igor Doroshenko <oknehsorod@gmail.com>
2023-10-09 19:26:49 +0300

    Add fzf plugin

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,9 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = true,
+  keys = {
+	{ '<C-f>', function () require('fzf-lua').live_grep() end, desc = 'Grep' }
+  },
+  opts = {}
+}
