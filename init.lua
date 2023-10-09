7150ddfbdcbaed234ba44aa6d3161253654fd7da
Igor Doroshenko <oknehsorod@gmail.com>
2023-10-09 08:54:03 +0300

    Add nvim-tree and gruvbox plugins

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
+require'editor-conf'
 require'lazy-setup'
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,8 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  keys = {
+    { '<C-n>', ':NvimTreeToggle<cr>', desc = 'Toggle NvimTree' }
+  },
+  name = 'nvim-tree',
+  opts = {}
+}
