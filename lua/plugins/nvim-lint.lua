400feb6578c2760d0b2b27616e1152031f69bf8f
Igor Doroshenko <oknehsorod@gmail.com>
2023-11-06 13:56:02 +0200

    Add nvim lint plugin

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,17 @@
+return {
+  'mfussenegger/nvim-lint',
+  lazy = false,
+  config = function ()
+    require('lint').linters_by_ft = {
+      typescript = {'eslint'},
+      javascript = {'eslint'},
+      typescriptreact = {'eslint'},
+      javascriptreact = {'eslint'},
+    }
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+  end
+}
